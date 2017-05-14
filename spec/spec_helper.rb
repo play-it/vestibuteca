b11265288fa7e1203ef072928321ddfcbf533a6d
Alexandre Saldanha <alexandre.bsaldanha@gmail.com>
2017-05-14 15:28:12 -0300

    Change Attachments format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,11 +23,11 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.default_formatter = "doc" if config.files_to_run.one?
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.filter_run_when_matching :focus
   config.example_status_persistence_file_path = "spec/examples.txt"
   config.disable_monkey_patching!
-  config.default_formatter = "doc"
   config.profile_examples = 10
   config.order = :random
 
